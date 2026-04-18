cf299f0000e4cec1b0c3149aac2f2ce33195e0ba
Ognjen_Pavlovic <ognjenpav41@gmail.com>
2026-04-18 23:47:43 +0200

    Fix socket CORS configuration

--- src/socket.js
+++ src/socket.js
@@ -5,5 +5,7 @@ const SOCKET_URL = import.meta.env.VITE_API_URL ||
                    (import.meta.env.MODE === 'production' ? "https://chat-app-backend-1-1.onrender.com" : "http://localhost:5000");
 
 export const socket = io(SOCKET_URL, {
-    autoConnect: false 
+    autoConnect: false,
+    transports: ["websocket"],
+    withCredentials: true 
 });
